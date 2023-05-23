aea28fdef173d496b697d79165325911c910ddd8
CamillaTH <hiltulacamilla@gmail.com>
2023-05-23 18:05:03 +0000

    added import of Globlas and HangmanHelper classes, created instance of hangmangHelper in init function

--- run.py
+++ run.py
@@ -1,9 +1,15 @@
+from Globals import Globals
+from HangmanHelper import HangmanHelper
+
 def init():
     """
     Function thats init the program and starts the game
     """
 
-    game_mode = input("Welcome to hangman! For multiplayer (your friend chooses a word) press 1 for singleplayer (computer chooses word) press 2 :\n")
+    # create instance of Drawings class to be able to visually show the user how many guesses there are left.
+    helper = HangmanHelper()
 
+    game_mode = input("Welcome to hangman! For multiplayer (your friend chooses a word) press 1 for singleplayer (computer chooses word) press 2 :\n")
+    
     
 init()
