03433548a8ab44b43f0dbe9a3444ee3d4d7649fe
CamillaTH <hiltulacamilla@gmail.com>
2023-05-23 18:14:29 +0000

    added static fucntion that validates the game choices input from the user

--- HangmanHelper.py
+++ HangmanHelper.py
@@ -2,3 +2,11 @@ class HangmanHelper:
     """
     Class that holds helper functions for the hangman game
     """
+
+    @staticmethod
+    def validate_game_choices(game_choice):
+        """
+       checks if input is 1 or 2 if not raise ValueError
+       :param game_choice:
+       :return: true if validates else false
+       """
