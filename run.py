abc824678a03f5fb23558a1a7d9982cdfa8f8a38
CamillaTH <hiltulacamilla@gmail.com>
2023-05-23 17:36:39 +0000

    using python3 input function get input from the user witch game mode the user want

--- run.py
+++ run.py
@@ -3,5 +3,7 @@ def init():
     Function thats init the program and starts the game
     """
 
+    game_mode = input("Welcome to hangman! For multiplayer (your friend chooses a word) press 1 for singleplayer (computer chooses word) press 2 :\n")
 
+    
 init()
