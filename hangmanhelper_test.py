245a5fecd1a408e0d4b92f5da97b1490353bf7c2
CamillaTH <hiltulacamilla@gmail.com>
2024-02-06 11:15:42 +0000

    added more tests for hangmanghelper

--- hangmanhelper_test.py
+++ hangmanhelper_test.py
@@ -10,5 +10,14 @@ class HangmanHelperTestFunctions(unittest.TestCase):
         self.assertTrue(HangmanHelper.validate_game_choices("2"))
 
     def test_validate_game_choices_invalid_input(self):
-         with patch('builtins.input', side_effect=["3"]):
-             self.assertFalse(HangmanHelper.validate_game_choices("3"))
+         with patch('builtins.input', side_effect=["6"]):
+             self.assertFalse(HangmanHelper.validate_game_choices("6"))
+
+    def test_validate_players_letter_choice_invalid_non_alphabetic(self):
+        self.assertFalse(HangmanHelper.validate_players_letter_choice("123"))
+
+    def test_validate_players_letter_choice_invalid_multiple_letters(self):
+        self.assertFalse(HangmanHelper.validate_players_letter_choice("abc"))
+
+    def test_validate_players_letter_choice_valid(self):
+        self.assertTrue(HangmanHelper.validate_players_letter_choice("b"))
